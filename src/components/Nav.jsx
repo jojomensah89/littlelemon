c895a6a1e06633ce0cf5557c6f079804cd10616d
Jojo Mensah <jojomensah89@gmail.com>
2023-07-14 15:25:06 +0000

    change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,38 @@
-import logo from '../icons_assets/Logo.svg'
-import { NavLink, Link } from 'react-router-dom';
-
+import logo from "../icons_assets/Logo.svg";
+import { NavLink, Link } from "react-router-dom";
 
 function Nav() {
-    return (
-        <nav className='nav'>
-            <img src={logo} alt="" />
-            <ul>
-                <li><Link to='/'>Home Page</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><NavLink to='/booking'>Reservations</NavLink></li>
-                <li><Link to='https://github.com/thmscly/little-lemon.git' rel="noopener noreferrer" target="_blank">Source Code</Link></li>
-                <li><Link to='/order'>Order Online</Link></li>
-                <li><Link to='/design'>Design</Link></li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="nav">
+      <img src={logo} alt="" />
+      <ul>
+        <li>
+          <Link to="/">Home Page</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <NavLink to="/booking">Reservations</NavLink>
+        </li>
+        <li>
+          <Link
+            to="https://github.com/jojomensah89/littlelemon"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Source Code
+          </Link>
+        </li>
+        <li>
+          <Link to="/order">Order Online</Link>
+        </li>
+        <li>
+          <Link to="/design">Design</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
